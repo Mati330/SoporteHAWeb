e428309933bac8148d78eb6b4f38427dd2aa6600
mati330 <matiasandres330@gmail.com>
2022-10-15 21:32:19 -0300

    se saca el comentario requirements de build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 # exit on error
 set -o errexit
 
-# poetry install <- este no, estoy usando requirements.txt ( que ya la pagina de render nos lo da)
 
 python manage.py collectstatic --no-input
 python manage.py migrate
