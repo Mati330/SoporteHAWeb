2802d48e8fa01ff62c628c6fe4e407577b320582
mati330 <matiasandres330@gmail.com>
2022-10-15 21:10:15 -0300

    requieremts comentado

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-# poetry install <- este no, estoy usando requirements.txt 
-pip freeze > requirements.txt
+# poetry install <- este no, estoy usando requirements.txt ( que ya la pagina de render nos lo da)
 
 python manage.py collectstatic --no-input
 python manage.py migrate
