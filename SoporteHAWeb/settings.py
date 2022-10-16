92eb60cda752bc79c56c309eda5be76dd3fefc5d
mati330 <matiasandres330@gmail.com>
2022-10-16 04:10:34 -0300

    se saca url ++

--- SoporteHAWeb/settings.py
+++ SoporteHAWeb/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'SoporteHAWeb.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite',        conn_max_age=600    )}
+    'default': dj_database_url.config()}
 
 #'ENGINE': 'django.db.backends.sqlite3',
 # #'NAME': BASE_DIR / 'db.sqlite3',
