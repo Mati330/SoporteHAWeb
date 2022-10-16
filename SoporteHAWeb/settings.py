f87e83d3f2da1d31546a82582a87fe920521cdba
mati330 <matiasandres330@gmail.com>
2022-10-16 01:42:13 -0300

    se agregaa render in apps de settings

--- SoporteHAWeb/settings.py
+++ SoporteHAWeb/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
+    
+    #render
+    'render.apps.RenderConfig',    
     #mi app
     'SoporteHAWebapp',
     
